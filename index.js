c04c98c7845f1264b86b25e11df3fcac7daa8fab
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-03-03 16:22:33 +0530

    updated

---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs')
+
+// fs.readFile('./index.txt', 'utf-8', (err, data)=>{
+//   if(err){
+//     console.error
+//     (err);   
+//   }
+//   console.log(data);
+  
+// })
+
+// fs.appendFile('new.txt', 'how are you \n',(err)=>{
+//   if(err){
+//     console.log(err);
+//   }
+//    console.log('File created');
+// })
+
+//rename file
+
+// fs.rename('./sample', './sample.txt',(err)=>{
+//   if(err){
+//     console.log(err);
+    
+//   }
+//   console.log('file renamed');
+  
+// })
+
+// delete file 
+
+// fs.unlink('./sample.txt', (err) => {
+//   if(err){
+//     console.log(err);
+//   }
+//   console.log('file deleted');
+// })
