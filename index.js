d062570ea3f1613337ba60607e347012ae014fc5
Muhammedminhaj798 <muhammedminhaj798@gmail.com>
2025-03-11 12:42:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var fs = require('fs')
+// var fs = require('fs')
+
+// const add = require('./math')
+
+
+// console.log(add(20,20));
 
 // fs.readFile('./index.txt', 'utf-8', (err, data)=>{
 //   if(err){
@@ -35,3 +40,43 @@ var fs = require('fs')
 //   }
 //   console.log('file deleted');
 // })
+
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req,res)=>{
+//     if(req.url === '/readFile'){
+//         fs.readFile('example.txt', 'utf8', (err,data)=>{
+//             if(err){
+//                 res.end('Your read file error')
+//             }else {
+//                 res.end(data)
+//             }
+//         })
+//     }else{
+//         res.end('route not found')
+//     }
+// })
+
+// server.listen(3000, () =>{
+//     console.log('server running');
+    
+// })
+
+// const http = require('http')
+// const fs = require('fs')
+
+// const server = http.createServer((req, res) => {
+//     if()
+// })
+
+
+// const fs = require('fs')
+// const http = require('http')
+
+// const server = http.createServer((req, res)=>{
+    
+// })
+
+const fs = require('fs')
+
